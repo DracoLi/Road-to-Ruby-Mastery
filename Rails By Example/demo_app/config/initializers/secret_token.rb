36441b707b4c398b18df597e585a0ae24a3f32fc
Draco Li <draco@dracoli.com>
2011-12-18 14:48:27 +0800

    Rails - chap 3

---
+++ Rails By Example/demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ce79a8b52c01046b560ed7adb11a177065a222b0a1193ef36447ae12e1608da527f6647912c18433d8be995b4feeacc8213abdad0b51fa1069323bd60575a850'
---
+++ Rails By Example/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9a765ab3e31b23192ec25218d1553b929edb3e8c56fdd52536e62fd8cf48bb82db8805a0d7586365d27808ddddcaddb89fc13e5886964b34355027df3a2ebc60'
